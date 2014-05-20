b036f24de3dd55cbccf492de5dbbc5f8da86b3d1
Quang <qhoang09@gmail.com>
2014-05-20 08:09:25 +0200

    Fin de l'application demo

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,8 @@
+
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: [:show, :edit, :update, :destroy]
 
+  # Validation des messages
   # GET /microposts
   # GET /microposts.json
   def index
